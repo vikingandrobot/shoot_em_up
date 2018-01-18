e0eb66ec3ce00b0752e97995fc55527779fb8f3e
Viking & Robot <vikingandrobot@gmail.com>
2018-01-18 12:08:29 +0100

    Commented Bullet.js

--- public/js/shoot_em_hub/Bullet.js
+++ public/js/shoot_em_hub/Bullet.js
@@ -1,14 +1,34 @@
+/**
+  File: Bullet.js
+  Authors: Mathieu Monteverde & Sathiya Kirsuhnapillai
+
+  The Bullet class represents a bullet. It has a position, a speed and a radius.
+*/
+
 class Bullet {
+
+  /**
+    Construct the bullet with initial position, speed and radius
+    pos: position (CartesianVector)
+    speed: speed (CartesianVector)
+    radius: radius of the bullet (integer or float)
+  */
   constructor(pos, speed, radius) {
     this.pos = pos.copy();
     this.speed = speed.copy();
     this.radius = radius;
   }
 
+  /**
+    Logic to move the bullet
+  */
   logic() {
     this.pos.add(this.speed);
   }
 
+  /*
+    Draw the bullet
+  */
   draw(ctx) {
     ctx.beginPath();
     ctx.fillStyle = 'white';
@@ -17,6 +37,11 @@ class Bullet {
     ctx.closePath();
   }
 
+  /*
+    Check whether the bullet is in collision with the given ennemy.
+    The ennemy object must have a pos {x, y} property, a w for width property
+    and a h for h property.
+  */
   collision(ennemy) {
     if (
       this.pos.x > ennemy.pos.x - ennemy.w / 2
