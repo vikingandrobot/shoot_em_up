7678cb64235366499491d91b9a856255421e686d
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-01-18 10:49:05 +0100

    Github connection with oauth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
+const GitHubApi = require('github');
+const request = require('request');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('../views/index');
+  res.render('../views/index', {clientId: process.env.GH_BASIC_CLIENT_ID});
+});
+
+router.get('/callback', (req, res) => {
+
+  // Get github code check
+  const code = req.query.code;
+
+  const data = {
+    client_id: process.env.GH_BASIC_CLIENT_ID,
+    client_secret: process.env.GH_BASIC_SECRET_ID,
+    code: code,
+  };
+
+  // Send back the code check
+  request.post({
+    headers: {'Accept':'application/json'},
+    url: 'https://github.com/login/oauth/access_token',
+    body: data,
+    json: true
+  }, (err, res, body) => {
+
+    // Get the token and store in session
+    req.session.token = body.access_token;
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 
 const express = require('express');
 const routes = require('./routes/index.js');
+const session = require('express-session');
+
+// Load environment variables
+require('dotenv').load();
 const port = process.env.PORT || 9090;
 
 const app = express();
 
+
 // Expose public stuff
 app.use(express.static('public'));
+
+// Use cookie for the sessions
+app.use(session({ 
+  secret: 'this-is-a-secret-token', 
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false
+}));
 
 // Set view engine
 app.set('view engine', 'ejs');
