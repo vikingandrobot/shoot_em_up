1b24a15b196b56390b339908e02ee955b7cd9ca9
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-01-20 18:55:11 +0100

    Add a linter

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "jquery": true,
+    "mocha": true,
+  },
+  "globals": {
+    "document": false,
+    "RepositorySelector": false,
+    "window": false,
+    "PlanetChart": false,
+    "Image": false,
+    "CartesianVector": false,
+    "PolarVector": false,
+    "Planet": false,
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": true,
+      }
+    ]
+  }
+};
