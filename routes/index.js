fc39a1771d030f7d20b9c40608f12958cdc06a45
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2018-01-22 21:39:01 +0100

    get skill

--- routes/index.js
+++ routes/index.js
@@ -147,4 +147,157 @@ router.get('/play', (req, res) => {
   res.render('../views/play');
 });
 
+router.get('/skills/:owner/:repo', (req, res) => {
+  // Check if the session is defined
+  if (req.session.token === undefined) {
+    res.status(401).send("Sorry need to be authenticated!");
+    return;
+  }
+
+  // Get the token from the session
+  const { token } = req.session;
+
+  const github = new GitHubApi();
+
+  // Try to connect with the token
+  github.authenticate({
+    type: 'oauth',
+    token,
+  });
+
+  const owner = req.params.owner;
+  const repo = req.params.repo;
+
+  // Get user data
+  function getUser(result) {
+    return new Promise((resolve, reject) => {
+      github.users.get({
+      }, (err, r) => {
+        // TODO : 401 ?
+        
+        result.login = r.data.login;
+        
+        resolve(result);
+      });
+    });
+  }
+
+  // Get total commits for the specified repository and user
+  function getTotalUserCommits(result) {
+    return new Promise((resolve, reject) => {
+      github.repos.getCommits({
+        owner,
+        repo,
+        author: result.login,
+        page: 1,
+        per_page: 1,
+      }, (err, r) => {
+        if (err && err.code === 404) {
+          return reject(new Error("Sorry can't find that owner/repo!"));
+        }
+
+        // Get response header
+        let nbCommits = 0;
+
+        if (r.meta && r.meta.link) {
+          const lastPageLink = r.meta.link.match(/page=(\d+)&.{65}; rel="last"/);
+          if (lastPageLink) {
+            nbCommits = Number(lastPageLink[1]);
+          }
+        } else {
+          if (r.data && r.data.length >= 1) {
+            nbCommits = 1;
+          }
+        }
+        
+        result.nbTotalUserCommits = nbCommits;
+
+        return resolve(result);
+      });
+    });
+  }
+
+
+
+  // Get total commits for the specified repository
+  function getTotalCommits(result) {
+    return new Promise((resolve, reject) => {
+      github.repos.getCommits({
+        owner,
+        repo,
+        page: 1,
+        per_page: 1,
+      }, (err, r) => {
+        if (err && err.code === 404) {
+          reject(new Error("Sorry can't find that owner/repo!"));
+        }
+
+        // Get response header
+        let nbCommits = 0;
+
+        if (r.meta && r.meta.link) {
+          const lastPageLink = r.meta.link.match(/page=(\d+)&.{65}; rel="last"/);
+          if (lastPageLink) {
+            nbCommits = Number(lastPageLink[1]);
+          }
+        } else {
+          if (r.data && r.data.length >= 1) {
+            nbCommits = 1;
+          }
+        }
+        
+        result.nbTotalCommits = nbCommits;
+
+        resolve(result);
+      });
+    });
+  }
+
+  // Get total contributors for the specified repository
+  function getTotalContributors(result) {
+    return new Promise((resolve, reject) => {
+      github.repos.getContributors({
+        owner,
+        repo,
+        page: 1,
+        per_page: 1,
+      }, (err, r) => {
+        if (err && err.code === 404) {
+          reject(new Error("Sorry can't find that owner/repo!"));
+        }
+
+        // Get response header
+        let nbContributors = 0;
+
+        if (r.meta && r.meta.link) {
+          const lastPageLink = r.meta.link.match(/page=(\d+)&.{65}; rel="last"/);
+          if (lastPageLink) {
+            nbContributors = Number(lastPageLink[1]);
+          }
+        } else {
+          if (r.data && r.data.length >= 1) {
+            nbContributors = 1;
+          }
+        }
+        
+        result.nbTotalContributors = nbContributors;
+
+        resolve(result);
+      });
+    });
+  }
+  
+  let result = {};
+  
+  getUser(result)
+    .then(getTotalUserCommits)
+    .then(getTotalCommits)
+    .then(getTotalContributors)
+    .then((r) => {
+      res.status(200).send("" + (r.nbTotalUserCommits / (r.nbTotalCommits / r.nbTotalContributors)));
+      return;
+    });
+  
+});
+
 module.exports = router;
